b81cad58f5f08a7d029e0429ef292e160129a3d2
aaranja <fidxle@gmail.com>
2022-04-29 06:14:09 -0700

    Auth actions and reducers for logging session added.

--- gui/src/store/actions/auth.js
+++ gui/src/store/actions/auth.js
@@ -1,3 +1,117 @@
-export const adminAuth = (values) =>{
+import {
+    AUTH_START,
+    AUTH_SUCCESS,
+    AUTH_FAIL,
+    AUTH_LOGOUT
+} from "../actionTypes";
+import axios from "axios";
+import {url} from "../url";
+import {sleep} from "../utils";
 
-}
+
+export const authStart = () => {
+    return {
+        type: AUTH_START,
+    };
+};
+
+export const authSuccess = (payload) => {
+    return {
+        type: AUTH_SUCCESS,
+        payload: payload,
+    };
+};
+
+export const authFail = (error) => {
+    return {
+        type: AUTH_FAIL,
+        error: error,
+    };
+};
+
+export const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expirationDate");
+    localStorage.removeItem("user_type");
+    localStorage.removeItem("all_name");
+    return {
+        type: AUTH_LOGOUT,
+    };
+};
+
+export const checkAuthTimeout = (expirationDate) => {
+    return (dispatch) => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationDate * 1000);
+    };
+};
+
+
+export const authAdmin = (email, password) => {
+    return (dispatch) => {
+        dispatch(authStart());
+        axios
+            .post(
+                `${url}/authenticate/`,
+                {
+                    username: email,
+                    password: password,
+                },
+                {
+                    headers: {
+                        "Accept-Language": "es-MX;",
+                    },
+                }
+            )
+
+            .then((res) => {
+                const token = res.data.token;
+                const user_type = res.data.user_type;
+                const all_name = res.data.fullname;
+
+                const expirationDate = new Date(new Date().getTime() + 3600 * 1000);
+                console.log("login aceptado: ".concat(expirationDate));
+                localStorage.setItem("token", token);
+                localStorage.setItem("expirationDate", expirationDate);
+                localStorage.setItem("user_type", user_type);
+                localStorage.setItem("all_name", all_name);
+                dispatch(
+                    authSuccess({token: token, usertype: user_type, name: all_name})
+                );
+                dispatch(checkAuthTimeout(3600));
+            })
+            .catch((err) => {
+                console.log(err.response)
+                dispatch(authFail(err.response.data));
+            });
+    };
+};
+
+export const authCheckState = () => {
+    return (dispatch) => {
+        dispatch(authStart());
+        const token = localStorage.getItem("token");
+        const user_type = localStorage.getItem("user_type");
+        const all_name = localStorage.getItem("all_name");
+        if (token === undefined) {
+            dispatch(logout());
+        } else {
+            const expirationDate = new Date(localStorage.getItem("expirationDate"));
+            if (expirationDate <= new Date()) {
+                dispatch(logout());
+            } else {
+                // Usage!
+                sleep(500).then(() => {
+                    const expirationTime =
+                        expirationDate.getTime() - new Date().getTime();
+                    const newDate = expirationTime / 1000;
+                    dispatch(
+                        authSuccess({token: token, usertype: user_type, name: all_name})
+                    );
+                    dispatch(checkAuthTimeout(newDate));
+                });
+            }
+        }
+    };
+};
--- gui/src/store/reducers/auth.js
+++ gui/src/store/reducers/auth.js
@@ -4,6 +4,7 @@ import {
     AUTH_LOGOUT,
     AUTH_FAIL,
 } from "../actionTypes";
+import {updateObject} from "../utils";
 
 const initialState = {
     payload: null,
@@ -13,19 +14,46 @@ const initialState = {
 };
 
 const authStart = (state) => {
-    return {error: null, loading: false, payload: null, status: null};
+    return updateObject(state, {
+        error: null,
+        loading: true,
+        status: "loading",
+    });
 }
+
+const authSuccess = (state, action) => {
+    return updateObject(state, {
+        payload: action.payload,
+        error: null,
+        loading: false,
+        status: "success",
+    });
+};
+
+const authFail = (state, action) => {
+    return updateObject(state, {
+        error: action.error,
+        loading: false,
+        status: "fail",
+    });
+};
+
+const authLogout = (state) => {
+    return updateObject(state, {
+        payload: null,
+    });
+};
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case AUTH_START:
             return authStart(state);
-        // case AUTH_SUCCESS:
-        //   return authSuccess(state, action);
-        // case AUTH_FAIL:
-        //   return authFail(state, action);
-        // case AUTH_LOGOUT:
-        //   return authLogout(state);
+        case AUTH_SUCCESS:
+            return authSuccess(state, action);
+        case AUTH_FAIL:
+            return authFail(state, action);
+        case AUTH_LOGOUT:
+            return authLogout(state);
         default:
             return state;
     }
