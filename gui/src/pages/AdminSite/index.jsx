815e0ac512fa501c0bac5a7bfd633ed5098eb0c5
aaranja <fidxle@gmail.com>
2022-05-01 14:03:51 -0700

    Redux state to auto try signup added, now it redirects to home admin site when is a token stored on localstorage. And logout function to header added.

--- gui/src/pages/AdminSite/index.jsx
+++ gui/src/pages/AdminSite/index.jsx
@@ -1,15 +1,77 @@
 import React, {Component} from "react";
 import {Outlet} from "react-router-dom";
 import SessionHeader from "../../components/SessionHeader";
+import {connect} from "react-redux";
+import {authCheckState, logout} from "../../store/actions/auth";
+import withNavigation from "../../routes/withRouter/withNavigation";
 
 class AdminSite extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            logged: false,
+        }
+    }
+
+    componentDidMount() {
+        this.props.onTryAutoSignup()
+    }
+
+    componentDidUpdate(prevProps: Readonly<P>, prevState: Readonly<S>, snapshot: SS) {
+        if (prevProps.status === "loading") {
+            const {status} = this.props;
+            if (status === "success") {
+                this.setState({
+                    logged: true,
+                })
+            } else {
+                if (status === "session_expired") {
+                    console.log("session expirada")
+                }
+            }
+
+        }
+    }
+
+    closeSession = () => {
+        this.props.logout()
+        this.setState({
+            logged: false,
+        })
+        this.props.navigate("/admin-site/login")
+    }
+
     render() {
         return <>
-            <SessionHeader/>
+            <SessionHeader logged={this.state.logged} closeSession={this.closeSession}/>
             <Outlet/>
-
         </>
     }
 }
 
-export default AdminSite;
+const mapStateToProps = (state) => {
+    const {auth} = state;
+    let authenticated = false;
+    if (auth.payload !== null) {
+        authenticated = auth.payload.token !== null
+    }
+
+    return {
+        status: state.auth.status,
+        authenticated: authenticated,
+
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onTryAutoSignup: () => {
+            dispatch(authCheckState())
+        },
+        logout: () => dispatch(logout()),
+    }
+}
+
+export default withNavigation(connect(mapStateToProps, mapDispatchToProps)(AdminSite));
