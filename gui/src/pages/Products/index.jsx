cc5719c03103578a884c03be555a9b0369f96fad
aaranja <fidxle@gmail.com>
2022-05-01 14:04:14 -0700

    Page to display a product crud added

---
+++ gui/src/pages/Products/index.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import {Container} from "react-bootstrap";
+
+class Products extends Component {
+    render() {
+        return <Container><span>CRUD productos</span></Container>
+    }
+}
+
+export default Products;
