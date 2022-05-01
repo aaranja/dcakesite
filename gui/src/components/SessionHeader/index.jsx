05def0015d522491c4f97511fcab17eb53e44649
aaranja <fidxle@gmail.com>
2022-05-01 13:56:21 -0700

    Links to security, page confugration, client orders and productos, and logout function added.

--- gui/src/components/SessionHeader/index.jsx
+++ gui/src/components/SessionHeader/index.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Container, Nav, Navbar} from 'react-bootstrap';
+import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap';
 import {Link} from "react-router-dom"
 import NavbarCollapse from "react-bootstrap/NavbarCollapse";
 
@@ -9,6 +9,25 @@ class SessionHeader extends Component {
             <Container>
                 <Link to="" className="navbar-brand">Pastelería Bibys</Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                {
+                    this.props.logged ? <NavbarCollapse id="basic-navbar-nav" className="justify-content-end">
+                        <Nav>
+                            <Link to="client-orders" className="nav-link">Pedidos</Link>
+                            <Link to="products" className="nav-link">
+                                Productos
+                            </Link>
+                            <Link to="page-configuration" className="nav-link">
+                                Presentación
+                            </Link>
+                            <NavDropdown title="Sesión" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="">Seguridad</NavDropdown.Item>
+                                <NavDropdown.Divider/>
+                                <NavDropdown.Item href="" onClick={this.props.closeSession}>Cerrar
+                                    sesión</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </NavbarCollapse> : null
+                }
             </Container>
         </Navbar>
     }
